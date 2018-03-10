0aa0182192a4523fc47e8e32c097c4457f5ca733
tangjw <tjwabc@gmail.com>
2018-03-10 16:52:02 +0800

    Initial commit

---
+++ app/src/main/java/com/zonsim/dagger2_demo/demo1/Student.java
@@ -0,0 +1,12 @@
+package com.zonsim.dagger2_demo.demo1;
+
+/**
+ * desc
+ *
+ * @author tangjunwei
+ * @email tjwabc@gmail.com
+ * Created by home-pc on 2018/3/10.
+ */
+
+public class Student {
+}
---
+++ app/src/main/java/com/zonsim/dagger2_demo/demo1/readme.md
@@ -0,0 +1,3 @@
+# Dagger2
+## @Inject 
+在Dagger 2中，使用javax.inject.Inject注解来标识需要依赖注入的构造函数和字段，以满足Dagger构造应用应用程序类的实例并满足其依赖性。
